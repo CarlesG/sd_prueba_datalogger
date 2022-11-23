40f465da13c30f61d7378686f0816563507e9da0
Carles Gallardo Llopis <cargall2@alumno.upv.es>
2022-11-23 12:14:56 +0100

    Tested termopar imiplementation

--- read_temperature/prueba_temperatura/prueba_temperatura.ino
+++ read_temperature/prueba_temperatura/prueba_temperatura.ino
@@ -1,7 +1,8 @@
 #include <math.h>
-#define Vcc 5;
-#define R10K 10000;
-#define pinSensorAnalog 0;
+#define Vcc 5
+#define R10K 10000
+#define pinSensorAnalog 0
+#define time_delay 1000
 
 float k = 2.5;
 
@@ -12,19 +13,30 @@ float C = 6.954079529e-8;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600)
+  Serial.begin(9600);
 }
 
 void loop() {
   float raw, V, R, logR, R_th, T_kelvin, T_celsius;
   // Obtenemos el valor de la resistencia a partir de la lectura de la tensión sobre ella:
   raw = analogRead(pinSensorAnalog);
-  V = raw / Vcc * 1024;
+  V = (raw / 1024) * Vcc;
   R = (R10K * V) / (Vcc - V);
   logR = log(R);
 
   // Modelo de Stenhart - Hart
-  R_th = 1.0 / (A + B * logR + C * logR * logR * logR)
+  R_th = 1.0 / (A + B * logR + C * logR * logR * logR);
 
   // Compensación de autocalentamiento
+  T_kelvin = R_th - ((V * V)/(k * R));
+  T_celsius = T_kelvin - 273.15;
+
+  // Imprimimos el resultado por pantalla
+  Serial.print("T = ");
+  Serial.print(T_celsius);
+  Serial.print(" C\n");
+  delay(time_delay);
 }
+
+
+
