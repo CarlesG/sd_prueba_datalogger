0ffa3f89457acc2f6cd052d4b1ed003552c3df9a
Carles Gallardo Llopis <cargall2@alumno.upv.es>
2022-11-25 18:53:16 +0100

    Added implementation of 3 temperature in the same digital pin

---
+++ read_temperature/prueba_temperatura/prueba_temperatura_ds18b20_encontrar_direccion/prueba_temperatura_ds18b20_encontrar_direccion.ino
@@ -0,0 +1,40 @@
+/* 
+ESTE PROGRAMA SIRVE PARA AVERIGUAR LA DIRECCIÓN DE CADA SENSOR, CONECTADO AL PIN CORRESPONDIENTE ONE - WIRE
+En este caso el pin 2 digital.
+
+- Librerías necesarias:
+-- One - wire: https://github.com/milesburton/Arduino-Temperature-Control-Library
+-- DallasTemperature: https://github.com/milesburton/Arduino-Temperature-Control-Library
+Para más información de la libreria, mirar el siguiente enlace: http://www.milesburton.com/Dallas_Temperature_Control_Library
+
+CONFIGURACIÓN PUERTOS ARDUINO
+-----------------------------
+
+DIGITALES:
+2 : sensor con cápsula acero inoxidable. y sensor sin capsula DS18B20
+*/
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+OneWire ourWire(2); // el pin2 será el bus OneWire
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  byte addr[8]; // array de 8 bytes, recordar que son direcciones de 64 bits que son 8 bytes 
+  Serial.println("Obteniendo direcciones:");
+  while(ourWire.search(addr)){
+    Serial.print("Address = ");
+    for(int i = 0; i < 8 ; i++){
+      Serial.print(" 0x");
+      Serial.print(addr[i], HEX);
+    }
+    Serial.println();
+  }
+  Serial.println();
+  ourWire.reset_search();
+  delay(2000);
+}
